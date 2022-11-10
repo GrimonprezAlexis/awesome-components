22466c1e49a524c42f56f9a6a116054b4c7f7308
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2022-11-10 16:06:36 +0100

    Use BrowserAnimationsModule and trigger animations

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     SharedModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   exports: [
     HeaderComponent
