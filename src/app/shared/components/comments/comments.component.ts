b562cbd28c86f44711e80b4bdf014dd34a89ec42
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2022-11-11 01:50:13 +0100

    Add trigger animation list with sequence group

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, animateChild, group, query, sequence, stagger, state, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
@@ -8,42 +8,76 @@ import { Comment } from 'src/app/core/models/comment.model';
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
     animations: [
-    trigger('listItem', [
-      state('default', style({
-        transform: 'scale(1)',
-        'background-color': 'white',
-        'z-index': 1
-      })),
-      state('active', style({
-        transform: 'scale(1.05)',
-        'background-color': 'rgb(201, 157, 242)',
-        'z-index': 2
-      })),
-      transition('default => active', [
-        animate('100ms ease-in-out')
+      trigger('list', [
+          transition(':enter', [
+              query('@listItem', [
+                  stagger(50, [
+                      animateChild()
+                  ])
+              ])
+          ])
       ]),
-      transition('active => default', [
-        animate('200ms')
-      ]),
-      transition('deleting => *', [
-        animate('500ms ease-in-out')
-      ]),
-      transition('* => deleting', [
-        animate('2000ms ease-out')
-      ]),
-      transition(':enter', [
-          style({
-              transform: 'translateX(-100%)',
-              opacity: 0,
-              'background-color': 'rgb(201, 157, 242)',
-          }),
-          animate('250ms ease-out', style({
-              transform: 'translateX(0)',
-              opacity: 1,
-              'background-color': 'white',
-          }))
+      trigger('listItem', [
+        state('default', style({
+          transform: 'scale(1)',
+          'background-color': 'white',
+          'z-index': 1
+        })),
+        state('active', style({
+          transform: 'scale(1.05)',
+          'background-color': 'rgb(201, 157, 242)',
+          'z-index': 2
+        })),
+        transition('default => active', [
+          animate('100ms ease-in-out')
+        ]),
+        transition('active => default', [
+          animate('200ms')
+        ]),
+        transition('deleting => *', [
+          animate('500ms ease-in-out')
+        ]),
+        transition('* => deleting', [
+          animate('2000ms ease-out')
+        ]),
+        transition(':enter', [
+            query('.comment-text, .comment-date', [
+                style({
+                    opacity: 0
+                }),
+            ]),
+            style({
+                transform: 'translateX(-100%)',
+                opacity: 0,
+                'background-color': 'rgb(201, 157, 242)',
+            }),
+            animate('250ms ease-out', style({
+                transform: 'translateX(0)',
+                opacity: 1,
+                'background-color': 'white',
+            })),
+            group([
+                sequence([
+                    animate('250ms', style({
+                        'background-color': 'rgb(255,7,147)'
+                    })),
+                    animate('250ms', style({
+                        'background-color': 'white'
+                    })),
+                ]),
+                query('.comment-text', [
+                    animate('250ms', style({
+                        opacity: 1
+                    }))
+                ]),
+                query('.comment-date', [
+                    animate('500ms', style({
+                        opacity: 1
+                    }))
+                ]),
+            ]),
+        ])
       ])
-    ])
   ]
 })
 export class CommentsComponent implements OnInit {
